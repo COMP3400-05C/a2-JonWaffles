89ecab11f1e012dc64b133dd4bb0941d53d6237b
Jonathan Hua-Phan <144959315+JonWaffles@users.noreply.github.com>
2025-10-10 16:20:17 -0400

    Update array_fill.c

--- array_fill.c
+++ array_fill.c
@@ -1,8 +1,21 @@
-#include <stddef.h> // For NULL
+#include <stddef.h>
 
-/**
- * TODO: Describe what the function does
- */
 int array_fill(int* array, int array_len, int begin, int end) {
-    return 1;
+    if (array == NULL || array_len <= 0) {
+        return 0;
+    }
+    if (begin > end) {
+        return 0;
+    }
+
+    int current = begin;
+    int filled = 0;
+
+    while (filled < array_len && current <= end) {
+        array[filled] = current;
+        ++filled;
+        ++current;
+    }
+
+    return filled;
 }
